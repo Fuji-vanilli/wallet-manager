0f23331e403604791328cdd40b82dd4dcb736009
Fuji-vanilli <ijufillivan@gmail.com>
2024-09-25 20:05:24 +0300

    update

--- wallet-service/src/main/java/com/fuji/wallet_service/services/WalletServiceImpl.java
+++ wallet-service/src/main/java/com/fuji/wallet_service/services/WalletServiceImpl.java
@@ -13,10 +13,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Stream;
 
 @Service
@@ -29,11 +26,12 @@ public class WalletServiceImpl implements WalletService {
 
     @Override
     public void loadCurrencies() {
-        Path path = Path.of("currencies.data.csv");
+        Path path = Path.of("H:\\currency-manager\\wallet-service\\src\\main\\resources\\currencies.data.csv");
+
 
         try {
             Files.readAllLines(path).stream()
-                    .filter(line-> line.startsWith("CODE"))
+                    .filter(line-> !line.startsWith("CODE"))
                     .forEach(line-> {
                         String[] currencyLines = line.split(";");
                         Currency currency= Currency.builder()
@@ -51,18 +49,22 @@ public class WalletServiceImpl implements WalletService {
 
         log.info("data loaded and add to the database successfully");
 
-        Stream.of("AR", "USD", "CAD", "EUR", "DIN").forEach(currencyCode-> {
+        Stream.of("AMD", "USD", "CAD", "EUR", "AZN").forEach(currencyCode-> {
             Currency currency= currencyRepository.findById(currencyCode).orElseThrow(
                     ()-> new IllegalArgumentException(String.format("currency %s doesn't exist", currencyCode))
             );
 
-            Wallet.builder()
+            Random random = new Random();
+            Wallet wallet= Wallet.builder()
                     .id(UUID.randomUUID().toString())
                     .currency(currency)
-                    .amount(new BigDecimal(5000))
+                    .amount(new BigDecimal(random.nextInt(5000, 15000)))
                     .createdAt(new Date())
                     .userID("USER_X")
                     .build();
+
+            walletRepository.save(wallet);
+            log.info("wallets added into the database");
         });
 
     }
