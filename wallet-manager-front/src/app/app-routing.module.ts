b4eb6857902af4f71c04dac278b37c22f9447c7a
Fuji-vanilli <ijufillivan@gmail.com>
2024-09-27 15:30:23 +0300

    update

--- wallet-manager-front/src/app/app-routing.module.ts
+++ wallet-manager-front/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, children: [
+  { path: 'admin', component: HomeComponent, children: [
+    { path: 'dashboard', component: DashboardComponent },
     { path: 'account', component: AccountComponent }
   ] }
 ];
--- wallet-manager-front/src/app/app.module.ts
+++ wallet-manager-front/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HomeComponent,
-    AccountComponent
+    AccountComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
